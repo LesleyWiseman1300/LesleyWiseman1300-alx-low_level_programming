582240801c696f0d4ac0f06a58edc9bce5cdd077
LesleyWiseman1300 <lesleywiseman9@gmail.com>
2023-02-22 04:19:12 +0200

    sleep

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
+/**
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 int main(void)
 {
-char characters[8] = "_putchar";
-int i = 0;
-for (i = 0 ; i < 9; i++)
-{
-putchar(characters[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
